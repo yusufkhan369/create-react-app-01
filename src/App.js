e6e8a19627081bf648491266b0e70742012743a1
yusufk999 <mdyusufk999@gmail.com>
2023-05-06 17:42:18 +0530

    Added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      My ToDo applicaiton updated
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent></ThirdComponent>
     </div>
   );
+}
+
+function FirstComponent() {
+
+  return (
+    <div className="FirstComponent">First Component</div>
+  )
+
+}
+
+function SecondComponent() {
+  return (
+    <div className='SecondComponent'> Second Component</div>
+  )
+}
+
+class ThirdComponent extends Component {
+  render() {
+    return (<div className='ThirdComponent'>Third Component</div>)
+  }
 }
 
 export default App;
