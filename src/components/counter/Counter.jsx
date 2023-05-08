c47d1647989653de179041c2c7d3cb3a4cb2c528
yusufk999 <mdyusufk999@gmail.com>
2023-05-08 06:19:11 +0530

    Added counter state

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,18 +1,27 @@
+import { useState } from 'react'
 import './Counter.css'
 
 export default function Counter() {
 
+    const [count, setCount] = useState(0);
+
     function incrementCounterFunction() {
-        console.log('increment clicked')
+        setCount(count + 1)
+    }
+
+    function decrementCounterFunction() {
+        setCount(count - 1)
     }
 
     return (
 
         < div className="Counter" >
-            <span className="count">0</span>
+            <span className="count">{count}</span>
             <div>
                 <button className="counterButton" onClick={incrementCounterFunction}
                 >+1</button>
+                <button className="counterButton" onClick={decrementCounterFunction}
+                >-1</button>
             </div>
         </div >
 
