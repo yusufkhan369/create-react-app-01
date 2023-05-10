d3a775a8c4cabf98099ce7a6ccdc8082aec6ff3e
yusufk999 <mdyusufk999@gmail.com>
2023-05-10 09:49:25 +0530

    Added Login Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-import ToDoApp from './components/todo/ToDoApp'
+import TodoApp from './components/todo/TodoApp'
 
 
 
 function App() {
   return (
     <div className="App">
-      <ToDoApp />
+      <TodoApp />
     </div>
   );
 }
--- src/components/todo/ToDoApp.jsx
+++ src/components/todo/ToDoApp.jsx
@@ -1,7 +1,36 @@
-export default function ToDoApp() {
+export default function TodoApp() {
     return (
+        <div className="TodoApp">
+            <LoginComponent />
+            {/* <WelcomeComponent /> */}
+        </div>
+    )
+}
 
-        <div className="ToDoApp">ToDo Management Application</div>
+function LoginComponent() {
+    return (
+        <div className="Login">
+            <div className="LoginForm">
+                <div>
+                    <label>User Name:</label>
+                    <input type="text" name="username" />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input type="password" name="password" />
+                </div>
+                <div>
+                    <button type="button" name="login">login</button>
+                </div>
+            </div>
+        </div>
+    )
+}
 
+function WelcomeComponent() {
+    return (
+        <div className="Welcome">
+            Welcome Component
+        </div>
     )
 }
